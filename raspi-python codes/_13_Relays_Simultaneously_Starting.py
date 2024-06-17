de6119042a3780f463177d126aa193f27ee413fb
avdhutssh <avdhut.ssh@gmail.com>
2024-06-17 13:33:31 +0530

    #Program to start relays simutaneously

--- raspi-python codes/_13_Relays_Simultaneously_Starting.py
+++ raspi-python codes/_13_Relays_Simultaneously_Starting.py
@@ -1,1 +1,20 @@
-#Program to start all relays simultaneously
+#Program to start all relays simultaneously on Three Channel RPi Relay Board Raspberry Pi
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+try:
+    GPIO.output(21, True)
+    GPIO.output(20, True)
+    GPIO.output(26, True)
+    time.sleep(1)  # Adjust the duration as needed
+    GPIO.output(21, False)
+    GPIO.output(20, False)
+    GPIO.output(26, False)
+finally:
+    GPIO.cleanup()
