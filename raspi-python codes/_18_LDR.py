46be7b25c4049421bdf98a060e663f540433b825
avdhutssh <avdhut.ssh@gmail.com>
2024-06-17 17:02:50 +0530

    Program to print resistance from a LDR

---
+++ raspi-python codes/_18_LDR.py
@@ -0,0 +1,17 @@
+#Program to print resistance from a LDR
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+pin=21
+while True:
+    reading=0
+    GPIO.setup(pin,GPIO.OUT)
+    GPIO.output(pin,GPIO.LOW)
+    time.sleep(1)
+    GPIO.setup(pin,GPIO.IN)
+    while(GPIO.input(pin)==GPIO.LOW):
+        reading = reading + 1
+    print (reading)
+    time.sleep(1)
